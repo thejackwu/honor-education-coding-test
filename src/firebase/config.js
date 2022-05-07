657fa06c642b73bd42f4312c892138299b390363
Jack Wu <jackwu833016@gmail.com>
2022-05-07 18:47:21 -0400

    Initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDoRLkUov-ZyQLeuQB00T4BI0cITuNxV80",
+  authDomain: "temp-project-eb4da.firebaseapp.com",
+  projectId: "temp-project-eb4da",
+  storageBucket: "temp-project-eb4da.appspot.com",
+  messagingSenderId: "874144806048",
+  appId: "1:874144806048:web:ff5948ba3fcba638ea45c5"
+};
